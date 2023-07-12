2fdd1da6960d32d54b2668b8799247aaae90a59a
석혜민 <lenaminn@naver.com>
2023-07-12 14:36:52 +0900

    edit gitignore

---
+++ Nanimo/Nanimo/View/Main/MainViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MainViewController.swift
+//  Nanimo
+//
+//  Created by Lena on 2023/07/12.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    private let minuteLabel: UILabel = {
+        let label = UILabel()
+        
+        return label
+    }()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+    
+}
