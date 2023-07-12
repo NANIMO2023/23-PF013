6b45747e09f760f814d83b5e76add9cbdc6749bc
석혜민 <lenaminn@naver.com>
2023-07-12 12:15:23 +0900

    create extensions (view, color)

---
+++ Nanimo/Nanimo/Global/Literal.swift
@@ -0,0 +1,11 @@
+//
+//  Literal.swift
+//  Nanimo
+//
+//  Created by Lena on 2023/07/12.
+//
+
+import UIKit
+
+let screenWidth = UIScreen.main.bounds.width
+let screenHeight = UIScreen.main.bounds.height
