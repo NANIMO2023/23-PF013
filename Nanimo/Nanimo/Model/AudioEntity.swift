38c007f1caba129a9a7020a7e4221e802fb3d591
lenamin <lenaminn@gmail.com>
2023-07-27 21:56:44 +0900

    [ADD] AudioEntity 추가 (#1)

---
+++ Nanimo/Nanimo/Model/AudioEntity.swift
@@ -0,0 +1,23 @@
+//
+//  AudioEntity.swift
+//  Nanimo
+//
+//  Created by Lena on 2023/07/27.
+//
+
+import Foundation
+
+struct AudioEntity: Identifiable {
+    var id = UUID().uuidString
+    
+    let kind: String
+    let decibel: Double
+    
+    let startHour: Int
+    let startMinute: Int
+    let startSecond: Int
+    
+    let endHour: Int
+    let endMinute: Int
+    let endSecond: Int
+}
